4d21df3d800c809402042f1a51cba19230453689
Iryna Popova <popovaira969@gmail.com>
2023-03-14 22:48:10 +0200

    finish

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,23 @@
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css'
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return options.map(option => {
     return (
-      <button className= "button" onClick={onLeaveFeedback} name = {option} key = {nanoid()}>
-        {option}
+      <button
+        className={css.button}
+        onClick={onLeaveFeedback}
+        name={option}
+        key={nanoid()}
+      >
+        {option.charAt(0).toUpperCase() + option.slice(1)}
       </button>
     );
   });
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
